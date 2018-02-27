1bab56982aef2d69a6c270e4cf16c5d1aeb56bfb
LintsDev <tsorakoto@gmail.com>
2018-02-27 17:08:10 +0300

    commit cookie - publicite

--- app/Publicite.php
+++ app/Publicite.php
@@ -7,10 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class Publicite extends Model
 {
 	protected $fillable = ['type','libelle','codecontenu','created_at','updated_at'];
-    //
-
+    
+	/**
+	* fonction get all publicites 
+	*/
     public function getallPublicite()
     {
     	return self::all();
     }
+
+    /**
+    * fonction get publicite selon page 
+    * @param string $type
+    * @return Collection Object Publicite
+    */
+    public function getpubBy($type)
+    {
+    	return self::where('type',$type)->first();
+    }
+
 }
