eeff2fbd368beaaab66b31651704296410596ea9
Liantso rakotoarinia <tsorakoto@gmail.com>
2018-04-11 17:25:02 +0300

    fin

--- resources/views/lstSiteInformation.blade.php
+++
@@ -1,6 +0,0 @@
-<p> liste des sites informations dans la base de données  </p>
-
-@foreach ($listSiteInformations as $siteInformation)
-    <p>Nandalo tato</p>
-    <p>{{ $siteInformation->NOMSITEWEB }}</p>
-@endforeach
